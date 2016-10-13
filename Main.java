93463a9eada7db627020ea10ddbcba05d9c0204a
Greg <g.j.lane90@gmail.com>
2016-10-12 21:33:46 -0400

    Project Complete

--- Main.java
+++ Main.java
@@ -3,9 +3,6 @@ package com.company;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.*;
 import java.util.*;
 import javax.swing.*;
 
@@ -13,89 +10,152 @@ import javax.swing.*;
 **********************************************************************************************************
 Author: Greg Lane
 Date: 10/06/16
-Project: project 3 - Program that Queries a student database and allows the option of adding and deleting
+Project: project 4 - Program that Queries a student database and allows the option of adding and deleting
 **********************************************************************************************************
  */
 
 
 public class Main extends JFrame {
 
+    // Build GUI objects and vars
+
+
+    private ArrayList<Student> al = new ArrayList<Student>();
+    private String studentName;
+    private String studentID;
+    private String studentMajor;
+    private String grade;
+    private int credit;
+    private String[] options1 = new String[]{"Insert", "Delete", "Find", "Update"};
+    private String[] options2 = new String[] {"A", "B", "C", "D", "F"};
+    private String[] options3 = new String[] {"3", "6"};
     private JOptionPane Frame = new JOptionPane();
     private JLabel id = new JLabel("id: ");
     private JLabel Name = new JLabel("Name: ");
     private JLabel Major = new JLabel("Major: ");
     private JLabel choose = new JLabel("Choose Selection: ");
     private JButton Process = new JButton("Process Request");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    private JComboBox choices = new JComboBox(options1);
+    private JComboBox Grade = new JComboBox(options2);
+    private JComboBox credits = new JComboBox(options3);
+    private JTextField idField = new JTextField("");
+    private JTextField nameField = new JTextField("");
+    private JTextField majorField = new JTextField("");
+    private Student theNewStudent = new Student();
+
+
+    // display
+
+    public void display(){
+        setVisible(true);
+    }
+
+    //Frame set
+
+    private void setFrame(int width, int height){
+        setLocationRelativeTo(null);
+        setSize(width, height);
+    }
+
+    public String getID(){
+        try{
+            return idField.getText();
+        }
+
+        catch(Exception e){
+            return "Error: " + e.getMessage();
+        }
+    }
+
+    public String getNameValue(){
+        try{
+            return nameField.getText();
+        }
+
+        catch(Exception e){
+            return "Error: " + e.getMessage();
+        }
+    }
+
+    public String getMajor(){
+        try{
+            return majorField.getText();
+        }
+
+        catch(Exception e){
+            return "Error: " + e.getMessage();
+        }
+    }
+
+    public void clearValue(){
+        idField.setText("");
+        nameField.setText("");
+        majorField.setText("");
+    }
+
+    public void setValues(){
+        this.studentName = getNameValue();
+        this.studentMajor = getMajor();
+        this.studentID = getID();
+    }
+
+    class ProcessButton implements ActionListener{ // Executes when button is clicked
+        @Override
+
+        public void actionPerformed(ActionEvent e){
+            setValues();
+            if(choices.getSelectedItem() == "Insert"){
+                theNewStudent.Insert(Integer.parseInt(studentID.toString()),studentName, studentMajor);
+                JOptionPane.showMessageDialog(Frame,"The student " + studentName + " Was entered successfully");
+
+            }
+            else if(choices.getSelectedItem() == "Delete"){
+
+                theNewStudent.delete(Integer.parseInt(studentID));
+
+            }
+            else if(choices.getSelectedItem() == "Find"){
+                theNewStudent.find(Integer.parseInt(studentID));
+            }
+
+            else if(choices.getSelectedItem() == "Update"){
+                JOptionPane.showMessageDialog(Frame,Grade,"Please select the grade",JOptionPane.QUESTION_MESSAGE);
+                grade = Grade.getSelectedItem().toString();
+                JOptionPane.showMessageDialog(Frame,credits,"Please select the credits",JOptionPane.QUESTION_MESSAGE);
+                credit = Integer.parseInt(credits.getSelectedItem().toString());
+                theNewStudent.courseCompleted(Integer.parseInt(studentID.toString()),grade,credit);
+
+            }
+
+            clearValue();
+        }
+    }
+
+
+    public Main(){
+        super("Student DB");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setFrame(350,250);
+        setResizable(false);
+        JPanel pan = new JPanel();
+        add(pan);
+        pan.setLayout(new GridLayout(5,2,0,10));
+        pan.add(id);
+        pan.add(idField);
+        pan.add(Name);
+        pan.add(nameField);
+        pan.add(Major);
+        pan.add(majorField);
+        pan.add(choose);
+        pan.add(choices);
+        pan.add(Process);
+        Process.addActionListener(new ProcessButton());
+        
+
+    }
 
     public static void main(String[] args) {
-	// write your code here
+	Main app = new Main();
+     app.display(); // Makes the app appear
     }
 }
--- Student.java
+++ Student.java
@@ -5,136 +5,123 @@ package com.company;
  */
 
 
+import javax.swing.*;
 import java.util.*;
 
 
 
 
 
-public class Student{
+public class Student extends JFrame {
 
+
+    private JOptionPane Frame = new JOptionPane();
     private String studentName;
     private int studentID;
     private HashMap studentMap = new HashMap();
     private HashMap MajorMap = new HashMap();
-    private HashMap gradeMap = new HashMap();
-    private HashMap creditMap = new HashMap();
     private HashMap gpaMap = new HashMap();
     private String major;
-    private double GPA;
-    private String grade;
-    private String credit;
-    private int creditNumeric;
-    private double gradeNumeric;
+    private double GPA = 0.00;
+    private int creditNumeric = 0;
+    private double gradeNumeric = 0;
+    private double gradeTemp;
+    private double tempGPA;
 
 
-    public Student(int studentID, String studentName, String major){
+    public Student() { // Empty constructor so methods can be accessed by creating one object in main
+
+
+    }
+
+    public void Insert(int studentID, String studentName, String major) { // INserts student into hashMap
+
         this.studentName = studentName;
         this.major = major;
         this.studentID = studentID;
-        studentMap.put(studentID, studentName);
-        MajorMap.put(studentID, major);
+        if (studentMap.containsKey(studentID)) {
+            JOptionPane.showMessageDialog(Frame, "This key already exists in the DB");
+        } else {
+            studentMap.put(studentID, studentName);
+        }
+
+        if (MajorMap.containsKey(studentID)) {
+
+        } else {
+            MajorMap.put(studentID, major);
+        }
+    }
+
+    public void courseCompleted(int studentID, String grade, int hours) { // Updates GPA
+        //this.studentName = studentName;
+        this.studentID = studentID;
+
+        if (grade.equals("A")) {
+            gradeTemp = 4.0;
+        } else if (grade.equals("B")) {
+            gradeTemp = 3.0;
+        } else if (grade.equals("C")) {
+            gradeTemp = 2.0;
+        } else if (grade.equals("D")) {
+            gradeTemp = 1.0;
+        } else {
+            gradeTemp = 0.0;
+        }
+
+        gradeNumeric = gradeTemp;
+
+        creditNumeric = hours;
+
+        tempGPA = (gradeTemp * creditNumeric) / creditNumeric;
+
+    }
+
+
+
+    public void delete(int key) { // Deletes user from hashmap
+        if (studentMap.containsKey(key)) {
+            studentMap.remove(key);
+            JOptionPane.showMessageDialog(Frame, "The student ID " + key + " has been removed");
+        } else {
+            JOptionPane.showMessageDialog(Frame, "Student does not exist in db");
+        }
+        if (MajorMap.containsKey(key)) {
+            MajorMap.remove(key);
+        } else {
+
+        }
+    }
+
+    public void find(int key) { // searches hashmap
+        if (studentMap.containsKey(key)) {
+            //studentName = studentMap.get(key).toString();
+            // major = MajorMap.get(key).toString();
+            JOptionPane.showMessageDialog(Frame, toString(key));
+        } else {
+            JOptionPane.showMessageDialog(Frame, "No key Found");
+        }
+
+    }
+
+    public String toString(int key) { // returns name, gpa and major
+
+        GPA = (gradeTemp * creditNumeric) / creditNumeric;
+        if(gpaMap.containsKey(studentID)){
+            gpaMap.remove(studentID);
+        }
+        else{
+            gpaMap.put(studentID, GPA);
+        }
+
+        studentName = studentMap.get(key).toString();
+        major = MajorMap.get(key).toString();
+
+        return "Student Name: " + studentName + " Major: " + major + " Current GPA: " + GPA;
+        }
 
 
     }
 
-    public void courseCompleted(String studentName,String grade, int hours){
-        this.studentName = studentName;
-        GPA = 0.0;
-        creditNumeric = 0;
-        gradeNumeric = 0;
-
-
-        if (grade.equals("A")){
-            gradeNumeric = 4.0;
-        }
-        else if (grade.equals("B")){
-            gradeNumeric = 3.0;
-        }
-
-        else if (grade.equals("C")){
-            gradeNumeric = 2.0;
-        }
-
-        else if (grade.equals("D")){
-            gradeNumeric = 1.0;
-        }
-        else{
-            gradeNumeric = 0.0;
-        }
-
-        if(gradeMap.isEmpty()){
-            gradeNumeric = gradeNumeric;
-        }
-        else {
-            gradeNumeric = gradeNumeric + Double.parseDouble(gradeMap.get(studentName).toString());
-        }
-
-
-        if (gradeMap.containsKey(studentName)){
-            gradeMap.remove(studentName);
-        }
-
-        gradeMap.put(studentName, gradeNumeric);
-
-            creditNumeric = hours;
-
-        if (creditMap.isEmpty()){
-
-        }
-        else{
-            creditNumeric = creditNumeric + Integer.parseInt(creditMap.get(studentName).toString());
-        }
-        if (creditMap.containsKey(studentName)){
-            creditMap.remove(studentName);
-
-        }
-
-        creditMap.put(studentName, creditNumeric);
-
-    }
-
-    public void calculateGPA(String studentName){
-        this.studentName = studentName;
-
-        if (gpaMap.isEmpty()){
-
-        }
-        else{
-            GPA = Integer.parseInt(creditMap.get(studentName).toString()) * Double.parseDouble(gradeMap.get(studentName).toString());
-        }
-
-        if(gpaMap.containsKey(studentName)){
-            gpaMap.remove(studentName);
-        }
-        gpaMap.put(studentName, GPA);
-
-    }
-
-   public void delete(int key){
-       studentMap.remove(key);
-       MajorMap.remove(key);
-   }
-
-   public void find(int key){
-       studentName = studentMap.get(key).toString();
-       major = MajorMap.get(key).toString();
-
-   }
-
-   public String toString(int key){
-       studentName = studentMap.get(key).toString();
-       major = MajorMap.get(key).toString();
-       GPA = Double.parseDouble(gpaMap.get(key).toString());
-       return "Student Name: " + studentName + " Major: " + major + " Current GPA: " + GPA;
-
-
-   }
-
-
-
-
-}
 
 
 
